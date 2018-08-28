df2e5bd5258556e8e69bfd5d833b69f987361cf1
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-28 10:16:41 -0400

    Added console log to visual baseline script

--- scripts/travis-visual-baselines.sh
+++ scripts/travis-visual-baselines.sh
@@ -2,6 +2,9 @@
 set -e
 
 # Update the webdriver-screenshots folder of the current branch, as long as it's a push and not a savage- branch.
+echo -e "Pull request? $TRAVIS_PULL_REQUEST"
+echo -e "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+echo -e "SAVAG_BRANCH: $SAVAGE_BRANCH"
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && ! $TRAVIS_BRANCH =~ $SAVAGE_BRANCH ]]; then
   echo -e "Checking new visual baseline images...\n"
   node ./node_modules/@blackbaud/skyux-builder-config/scripts/visual-baselines.js
