a045ee93e4aee8b282b323c4583889bbd6a8ec7c
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-28 09:56:03 -0400

    Added console log to visual baseline script

--- scripts/travis-visual-baselines.sh
+++ scripts/travis-visual-baselines.sh
@@ -3,6 +3,8 @@ set -e
 
 # Update the webdriver-screenshots folder of the current branch, as long as it's a push and not a savage- branch.
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && ! $TRAVIS_BRANCH =~ $SAVAGE_BRANCH ]]; then
-  echo -e "Checking visual baseline images...\n"
+  echo -e "Checking new visual baseline images...\n"
   node ./node_modules/@blackbaud/skyux-builder-config/scripts/visual-baselines.js
+else
+  echo -e "Visual baseline images are not saved during a pull request. Aborting.\n"
 fi
