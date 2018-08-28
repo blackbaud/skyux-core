8ab0abd1962b2c041663a9e8a6bdd75cb9a89289
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-28 10:50:15 -0400

    Removed newline characters from console logs

--- scripts/travis-visual-baselines.sh
+++ scripts/travis-visual-baselines.sh
@@ -2,8 +2,8 @@
 set -e
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  echo "Checking new visual baseline images...\n"
+  echo "Checking new visual baseline images..."
   node ./node_modules/@blackbaud/skyux-builder-config/scripts/visual-baselines.js
 else
-  echo "Visual baseline images are not saved during a pull request. Aborting script.\n"
+  echo "Visual baseline images are not saved during a pull request. Aborting script."
 fi
