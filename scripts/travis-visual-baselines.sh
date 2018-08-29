276d33cae6aec7c6e4c78cf00ae66afb3f18e371
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-29 12:20:46 -0400

    Implementing new visual test features

--- scripts/travis-visual-baselines.sh
+++
@@ -1,9 +0,0 @@
-# Fail the build if this step fails
-set -e
-
-if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  echo "Checking new visual baseline images..."
-  node ./node_modules/@blackbaud/skyux-builder-config/scripts/visual-baselines.js
-else
-  echo "Visual baseline images are not checked during a pull request. Aborting script."
-fi
