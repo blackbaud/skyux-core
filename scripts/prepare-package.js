486c9fcf3eb47203c96858dfd7dccd345817b645
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-09-12 12:40:12 -0400

    Copy utils to dist

--- scripts/prepare-package.js
+++ scripts/prepare-package.js
@@ -1,2 +1,9 @@
+const fs = require('fs-extra');
 const compat = require('../utils/node-js/compat');
+
+function copyUtils() {
+  fs.copySync('./utils', './dist/utils');
+}
+
 compat.injectLocaleResourcesContents();
+copyUtils();
