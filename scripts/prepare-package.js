8e138528d7ad3a206251352dfe6b836ecf067696
Steve Brush <steve.brush@blackbaud.com>
2018-09-12 17:26:20 -0400

    Revert "Statically inject resources contents (#29)" (#30)
    This reverts commit 4ad8cfc9ded15c2a663ca188da00901a97a21462.

--- scripts/prepare-package.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-const compat = require('../utils/node-js/compat');
-
-function copyUtils() {
-  fs.copySync('./utils', './dist/utils');
-}
-
-compat.injectLocaleResourcesContents();
-copyUtils();
