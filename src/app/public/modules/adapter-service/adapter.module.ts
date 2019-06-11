66ef7d987124a7b051fdaa3e17704df888b00bda
Alex Kingman <alex.kingman@blackbaud.com>
2019-06-11 10:55:39 -0400

    added sky adapter service

---
+++ src/app/public/modules/adapter-service/adapter.module.ts
@@ -0,0 +1,14 @@
+import {
+  NgModule
+} from '@angular/core';
+
+import {
+  SkyAdapterService
+} from './adapter.service';
+
+@NgModule({
+  providers: [
+    SkyAdapterService
+  ]
+})
+export class SkyAdapterModule { }
