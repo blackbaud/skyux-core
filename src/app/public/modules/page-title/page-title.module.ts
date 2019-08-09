76e9c6910f2c4e151b3a1c999970dd2b1706e683
Blackbaud-JackMcElhinney <39593127+Blackbaud-JackMcElhinney@users.noreply.github.com>
2019-08-09 16:29:37 -0400

    add page title service (#93)

---
+++ src/app/public/modules/page-title/page-title.module.ts
@@ -0,0 +1,14 @@
+import {
+  NgModule
+} from '@angular/core';
+
+import {
+  SkyPageTitleService
+} from './page-title.service';
+
+@NgModule({
+  providers: [
+    SkyPageTitleService
+  ]
+})
+export class SkyPageTitleModule { }
