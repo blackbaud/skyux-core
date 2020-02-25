6ef6874afd3d0df0e3996552eee9d077eaf8f935
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-02-25 17:12:25 -0500

    Code style

--- src/app/public/modules/dock/dock-insert-component-config.ts
+++ src/app/public/modules/dock/dock-insert-component-config.ts
@@ -1,6 +1,16 @@
-import { SkyDockItemConfig } from './dock-item-config';
-import { StaticProvider } from '@angular/core';
+import {
+  StaticProvider
+} from '@angular/core';
+
+import {
+  SkyDockItemConfig
+} from './dock-item-config';
 
 export interface SkyDockInsertComponentConfig extends SkyDockItemConfig {
+
+  /**
+   * Static providers to inject into the item's component.
+   */
   providers?: StaticProvider[];
+
 }
