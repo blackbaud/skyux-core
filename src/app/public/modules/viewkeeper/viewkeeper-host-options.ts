5b334c47e8aab623642113ca294348e6a23baee5
Paul Crowder <paul.crowder@blackbaud.com>
2020-03-04 10:10:59 -0500

    Added interface properties to `SkyViewkeeperHostOptions`
    This fixes an issue with the Angular AoT compiler since it doesn't recognize properties not declared on a class even if it implements an interface with those properties.

--- src/app/public/modules/viewkeeper/viewkeeper-host-options.ts
+++ src/app/public/modules/viewkeeper/viewkeeper-host-options.ts
@@ -7,4 +7,16 @@ import {
 } from './viewkeeper-options';
 
 @Injectable()
-export class SkyViewkeeperHostOptions implements SkyViewkeeperOptions { }
+export class SkyViewkeeperHostOptions implements SkyViewkeeperOptions {
+  public setWidth?: boolean;
+
+  public el?: HTMLElement;
+
+  public boundaryEl?: HTMLElement;
+
+  public verticalOffset?: number;
+
+  public verticalOffsetEl?: HTMLElement;
+
+  public viewportMarginTop?: number;
+}
