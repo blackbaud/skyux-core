56e41314dc30b7e383f68807222c7f20a3d18001
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-05-18 14:44:00 -0400

    Try script

---
+++ .github/workflows/karma.conf.js
@@ -0,0 +1,17 @@
+function getConfig(config) {
+  require('@skyux-sdk/builder/config/karma/shared.karma.conf')(config);
+
+  const configuration = {
+    browsers: ['Chrome_travis_ci'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    }
+  };
+
+  config.set(configuration);
+}
+
+module.exports = getConfig;
