838dd8bb0b5514140f039f76043787ba8cada8cf
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-05-18 15:02:20 -0400

    Fix

--- .github/workflows/karma.conf.js
+++ .github/workflows/karma.conf.js
@@ -2,7 +2,13 @@ function getConfig(config) {
   require('@skyux-sdk/builder/config/karma/shared.karma.conf')(config);
 
   const configuration = {
-    browsers: ['Chrome']
+    browsers: ['Chrome_travis_ci'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   };
 
   config.set(configuration);
