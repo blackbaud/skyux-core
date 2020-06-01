d4d1f099f793ddab435fa4523a621f0a69d08d0e
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-06-01 15:57:55 -0400

    Minimize complexity

--- scripts/karma.conf.js
+++ scripts/karma.conf.js
@@ -1,13 +1,29 @@
-const applyBuilderConfig = require('@skyux-sdk/builder/config/karma/shared.karma.conf');
-
 module.exports = (config) => {
 
-  applyBuilderConfig(config);
+  const minimist = require('minimist');
+  const testWebpackConfig = require('@skyux-sdk/builder/config/webpack/test.webpack.config');
+  const skyPagesConfigUtil = require('@skyux-sdk/builder/config/sky-pages/sky-pages.config');
+
+  const argv = minimist(process.argv.slice(2));
+  const skyPagesConfig = skyPagesConfigUtil.getSkyPagesConfig(argv._[0]);
+  const webpackConfig = testWebpackConfig.getWebpackConfig(skyPagesConfig, argv);
+
+  const specBundle = `${__dirname}/../node_modules/@skyux-sdk/builder/utils/spec-bundle.js`;
+  const specStyles = `${__dirname}/../node_modules/@skyux-sdk/builder/utils/spec-styles.js`;
+  const polyfillsBundle = `${__dirname}/../node_modules/@skyux-sdk/builder/src/polyfills.ts`;
+
+  const preprocessors = {};
+  preprocessors[polyfillsBundle] = ['webpack'];
+  preprocessors[specBundle] = ['webpack', 'sourcemap'];
+  preprocessors[specStyles] = ['webpack'];
+
+  const port = 9876;
 
   config.set({
     browserStack: {
       username: process.env.BROWSER_STACK_USERNAME,
-      accessKey: process.env.BROWSER_STACK_ACCESS_KEY
+      accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
+      port
     },
     customLaunchers: {
       bs_firefox_mac: {
@@ -18,6 +34,31 @@ module.exports = (config) => {
         os_version: 'Mountain Lion'
       }
     },
-    browsers: ['bs_firefox_mac']
+    browsers: ['bs_firefox_mac'],
+    port,
+    webpack: webpackConfig,
+    skyPagesConfig,
+    preprocessors,
+    files: [
+      {
+        pattern: polyfillsBundle,
+        watched: false
+      },
+      {
+        pattern: specBundle,
+        watched: false
+      },
+      {
+        pattern: specStyles,
+        watched: false
+      },
+      {
+        pattern: assetsPattern,
+        included: false,
+        served: true,
+      }
+    ],
+    basePath: '',
+    frameworks: ['jasmine']
   });
 };
