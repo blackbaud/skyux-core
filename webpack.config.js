5599905a7b9cce977e1479361ae2ef7f8341a353
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-06-04 11:20:39 -0400

    Remove more

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
-const tsLoaderUtil = require('@skyux-sdk/builder/config/webpack/ts-loader-rule');
 const path = require('path');
 const DefinePlugin = require('webpack/lib/DefinePlugin');
 const LoaderOptionsPlugin = require('webpack/lib/LoaderOptionsPlugin');
-
-function spaPath(...args) {
-  return path.join(process.cwd(), ...args);
-}
 
 function outPath(...args) {
   return path.join(process.cwd(), 'node_modules/@skyux-sdk/builder', ...args);
@@ -16,13 +11,9 @@ module.exports = {
 
     const srcPath = path.resolve(process.cwd(), 'src', 'app', 'public');
 
-    const excludes = [
-      spaPath('node_modules'),
-      outPath('node_modules')
-    ];
-
     return {
       mode: 'development',
+
       resolve: {
         extensions: ['.ts', '.js']
       },
@@ -31,31 +22,20 @@ module.exports = {
         rules: [
           {
             enforce: 'pre',
-            test: /runtime\/config\.ts$/,
-            loader: outPath('loader', 'sky-app-config')
-          },
-          {
-            enforce: 'pre',
             test: /sky-pages\.module\.ts$/,
             loader: outPath('loader', 'sky-pages-module')
           },
-          {
-            enforce: 'pre',
-            loader: outPath('loader', 'sky-processor', 'preload'),
-            exclude: excludes
-          },
-          {
-            enforce: 'pre',
-            test: /skyux-i18n-testing\.js$/,
-            loader: outPath('loader', 'sky-fix-require-context')
-          },
 
-          tsLoaderUtil.getRule(skyPagesConfig.runtime.command),
+          {
+            test: /\.ts$/,
+            use: ['awesome-typescript-loader', 'angular2-template-loader']
+          },
 
           {
             test: /\.s?css$/,
             use: ['raw-loader', 'sass-loader']
           },
+
           {
             test: /\.html$/,
             loader: 'raw-loader'
@@ -68,7 +48,7 @@ module.exports = {
           debug: true,
           options: {
             context: __dirname,
-            skyPagesConfig: skyPagesConfig
+            skyPagesConfig
           }
         }),
 
