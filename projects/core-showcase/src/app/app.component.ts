6d25fc6a3f253849529078f676cd1728dc1975f6
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2021-09-09 08:36:16 -0400

    Update app component, module

--- projects/core-showcase/src/app/app.component.ts
+++ projects/core-showcase/src/app/app.component.ts
@@ -1,26 +1,17 @@
-import {
-  Component,
-  Renderer2
-} from '@angular/core';
-
-import {
-  SkyTheme,
-  SkyThemeMode,
-  SkyThemeService,
-  SkyThemeSettings
-} from '@skyux/theme';
+import { Component, Renderer2 } from '@angular/core';
+import { SkyAppStyleLoader, SkyTheme, SkyThemeMode, SkyThemeService, SkyThemeSettings } from '@skyux/theme';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  public title = 'core-showcase';
+  public isLoaded = false;
 
   constructor(
     renderer: Renderer2,
-    themeService: SkyThemeService
+    themeService: SkyThemeService,
+    styleLoader: SkyAppStyleLoader
   ) {
     themeService.init(
       document.body,
@@ -30,5 +21,9 @@ export class AppComponent {
         SkyThemeMode.presets.light
       )
     );
+
+    styleLoader.loadStyles().then(() => {
+      this.isLoaded = true;
+    });
   }
 }
